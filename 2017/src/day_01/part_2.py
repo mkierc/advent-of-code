c42dd9eda5eecaf76c5f4b64fbc1255b4b568e62
mkierc <urban.pl@gmail.com>
2017-12-01 10:44:10 +0100

    solved part 2 of the day 1 challenge

---
+++ 2017/src/day_01/part_2.py
@@ -0,0 +1,46 @@
+number_list = []
+
+with open("data.txt") as file:
+    input_data = str(file.readline())
+    for character in input_data:
+        number_list.append(int(character))
+
+test_list_1 = [1, 2, 1, 2]
+test_list_2 = [1, 2, 2, 1]
+test_list_3 = [1, 2, 3, 4, 2, 5]
+test_list_4 = [1, 2, 3, 1, 2, 3]
+test_list_5 = [1, 2, 1, 3, 1, 4, 1, 5]
+
+
+def solve_captcha(numbers):
+    captcha_sum = 0
+    i = 0
+
+    while i < len(numbers):
+        a = numbers[i % len(numbers)]
+        b = numbers[int((i + (len(numbers) / 2))) % len(numbers)]
+        if a == b:
+            captcha_sum += a
+        i += 1
+
+    return captcha_sum
+
+
+def main():
+    test_1 = solve_captcha(test_list_1)
+    test_2 = solve_captcha(test_list_2)
+    test_3 = solve_captcha(test_list_3)
+    test_4 = solve_captcha(test_list_4)
+    test_5 = solve_captcha(test_list_5)
+    answer = solve_captcha(number_list)
+
+    print("test_1:", test_1)
+    print("test_2:", test_2)
+    print("test_3:", test_3)
+    print("test_4:", test_4)
+    print("test_5:", test_5)
+    print("answer:", answer)
+
+
+if __name__ == "__main__":
+    main()
