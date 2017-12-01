17bee940675c74156454860c37606172e5a0bd60
mkierc <urban.pl@gmail.com>
2017-12-01 10:43:37 +0100

    solved part 1 of the challenge, added the part 2 text

--- 2017/src/day_01/part_1.py
+++ 2017/src/day_01/part_1.py
@@ -5,4 +5,39 @@ with open("data.txt") as file:
     for character in input_data:
         number_list.append(int(character))
 
-print(number_list)
+test_list_1 = [1, 1, 2, 2]
+test_list_2 = [1, 1, 1, 1]
+test_list_3 = [1, 2, 3, 4]
+test_list_4 = [9, 1, 2, 1, 2, 1, 2, 9]
+
+
+def solve_captcha(numbers):
+    captcha_sum = 0
+    i = 0
+
+    while i < len(numbers):
+        a = numbers[i % len(numbers)]
+        b = numbers[(i + 1) % len(numbers)]
+        if a == b:
+            captcha_sum += a
+        i += 1
+
+    return captcha_sum
+
+
+def main():
+    test_1 = solve_captcha(test_list_1)
+    test_2 = solve_captcha(test_list_2)
+    test_3 = solve_captcha(test_list_3)
+    test_4 = solve_captcha(test_list_4)
+    answer = solve_captcha(number_list)
+
+    print("test_1:", test_1)
+    print("test_2:", test_2)
+    print("test_3:", test_3)
+    print("test_4:", test_4)
+    print("answer:", answer)
+
+
+if __name__ == "__main__":
+    main()
