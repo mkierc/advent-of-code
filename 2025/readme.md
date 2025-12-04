d7def7e093ceb0c42a7a32d6769080022c6480b3
mkierc <urban.pl@gmail.com>
2025-12-04 12:48:39 +0100

    solved 2025-04-1, 2025-04-2

--- 2025/readme.md
+++ 2025/readme.md
@@ -16,4 +16,8 @@ Starting this year, there are just 12 days of puzzles.
 
 #### [Day 3: Lobby](http://adventofcode.com/2025/day/3)
 - [x] [Part One - What is the total output joltage?](src/day_03/part_1.py)
-- [x] [Part Two - What is the new total output joltage? ?](src/day_03/part_2.py)
+- [x] [Part Two - What is the new total output joltage?](src/day_03/part_2.py)
+
+#### [Day 4: Printing Department](http://adventofcode.com/2025/day/3)
+- [x] [Part One - How many rolls of paper can be accessed by a forklift?](src/day_04/part_1.py)
+- [x] [Part Two - How many rolls of paper in total can be removed by the Elves and their forklifts?](src/day_04/part_2.py)
---
+++ 2025/src/day_04/part_1.py
@@ -0,0 +1,89 @@
+from copy import deepcopy
+from time import time
+
+test_input_1 = [
+    '..@@.@@@@.',
+    '@@@.@.@.@@',
+    '@@@@@.@.@@',
+    '@.@@@@..@.',
+    '@@.@@@@.@@',
+    '.@@@@@@@.@',
+    '.@.@.@.@@@',
+    '@.@@@.@@@@',
+    '.@@@@@@@@.',
+    '@.@.@@@.@.',
+]
+
+with open('data.txt') as file:
+    input_data = file.read().splitlines()
+
+
+def pprint(grid):
+    for y, line in enumerate(grid):
+        for x, char in enumerate(line):
+            print(char, end='')
+        print('')
+
+
+def extend_grid(grid: list, extent: int, character: str):
+    new_grid = deepcopy(grid)
+    width = len(grid[0])
+
+    for i, line in enumerate(new_grid):
+        padding = character * extent
+        line = padding + line + padding
+        new_grid[i] = line
+
+    for i in range(extent):
+        new_grid.insert(0, character * int(width + 2 * extent))
+    for i in range(extent):
+        new_grid.append(character * int(width + 2 * extent))
+
+    return new_grid
+
+
+def solve(grid):
+    extent = 1
+    pad = '.'
+    extended = extend_grid(grid, extent, pad)
+    redrawn = []
+    for y in range(len(extended)):
+        redrawn.append([])
+        for x in range(len(extended[0])):
+            redrawn[y].append(pad)
+
+    count = 0
+    for y, line in enumerate(extended[extent:-extent], start=extent):
+        for x, character in enumerate(line[extent:-extent], start=extent):
+            if character == '@':
+                adjacents = [
+                    extended[y - 1][x - 1],
+                    extended[y - 1][x],
+                    extended[y - 1][x + 1],
+                    extended[y][x - 1],
+                    extended[y][x + 1],
+                    extended[y + 1][x - 1],
+                    extended[y + 1][x],
+                    extended[y + 1][x + 1]
+                ]
+
+                if adjacents.count('@') < 4:
+                    redrawn[y][x] = 'X'
+                    count += 1
+
+    # pprint(redrawn)
+    return count
+
+
+def main():
+    test_1 = solve(test_input_1)
+    print('test_1:', test_1)
+
+    start = time()
+    answer = solve(input_data)
+    print('time:', time() - start)
+    print('answer:', answer)
+
+
+if __name__ == '__main__':
+    main()
